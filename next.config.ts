20189c25bcf0a88c15918b31245add0c9d75ad48
Gabriel Ado Ramos Tukan <gabrieladoramostukan@gmail.com>
2026-09-03 18:37:13 +0800

    updating next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	/* config options here */
-	typescript: {
-		ignoreBuildErrors: true,
-	},
+	// typescript: {
+	// 	ignoreBuildErrors: true,
+	// },
 };
 
 export default nextConfig;
